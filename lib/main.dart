b017a4d962eeb5840068cb3dc4593a832eeb3097
MananFin5911 <manan@finsoltech.com>
2025-06-27 23:04:18 +0530

    Registered ios app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
+import 'firebase_options.dart';
 import 'core/screens/auth/login_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MyApp()));
 }
 
